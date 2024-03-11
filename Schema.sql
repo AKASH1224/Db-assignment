7d51ecf17ac7456645edd54e14e78d93f4d15e88
Akash deep <77068840+AKASH1224@users.noreply.github.com>
2024-03-11 11:51:16 +0530

    Schema. sql

--- Schema.sql
+++ Schema.sql
@@ -1,1 +1,12 @@
 
+CREATE TABLE product_category (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE product (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  category_id INT FOREIGN KEY REFERENCES product_category(id),
+  -- Other product attributes
+);
